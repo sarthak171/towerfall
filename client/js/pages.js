f9d45e34612909a6e1ba8ecdae03d96c4aea3f3b
Sarthak Madan <sarthak.madan17@gmail.com>
2020-06-16 13:09:55 -0700

    rooms complete

--- client/js/pages.js
+++ client/js/pages.js
@@ -1,3 +1,36 @@
-function EndCss() {
+function clear() {
+  document.getElementById('box1').style.visibility = 'hidden';
+  document.getElementById('main').style.visibility = 'hidden';
   document.getElementById('form').style.visibility = 'hidden';
+  document.getElementById('ctrlspage').style.visibility = 'hidden';
+  document.getElementById('creditspage').style.visibility = 'hidden';
+}
+
+function mainShowCss() {
+  document.getElementById('box1').style.visibility = 'visible';
+  document.getElementById('main').style.visibility = 'visible';
+  document.getElementById('form').style.visibility = 'visible';
+}
+
+function startCss() {
+  clear();
+}
+
+function ctrlsShowCss(){
+  document.getElementById('form').style.visibility = 'hidden';
+  document.getElementById('ctrlspage').style.visibility = 'visible';
+}
+
+function ctrlsBackCss() {
+  document.getElementById('ctrlspage').style.visibility = 'hidden';
+  document.getElementById('form').style.visibility = 'visible';
+}
+
+function creditsShowCss() {
+  document.getElementById('form').style.visibility = 'hidden';
+  document.getElementById('creditspage').style.visibility = 'visible';
+}
+function creditsBackCss() {
+  document.getElementById('creditspage').style.visibility = 'hidden';
+  document.getElementById('form').style.visibility = 'visible';
 }
